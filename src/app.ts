e4d92ebb68ba5dc186da0e84003d4f96075cd0ac
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2023-07-11 18:20:58 +0200

    chore: add logger middleware in verbose mode

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
 
 import { init as initRoutes } from './routes'
 
@@ -7,4 +8,10 @@ export const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// logs all requests in verbose mode
+if (process.env.VERBOSE === 'true') {
+  app.use(morgan('tiny', { immediate: true }))
+}
+
 initRoutes(app)
