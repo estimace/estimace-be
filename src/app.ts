08c8b43e70a519f70d22e31bf3503971bcf1cf6c
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2023-07-15 13:40:16 +0200

    chore: remove body-parser dependency

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import morgan from 'morgan'
 
 import { init as initRoutes } from './routes'
 
 export const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // logs all requests in verbose mode
 if (process.env.VERBOSE === 'true') {
