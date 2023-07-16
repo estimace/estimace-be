4eff20846afe7f1fbb839532575d5c7a0930cbd7
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2023-07-16 19:52:51 +0200

    refactor: move attachWSToServer to wss directory

--- src/server.ts
+++ src/server.ts
@@ -1,54 +1,12 @@
 import { AddressInfo } from 'net'
 import { createServer } from 'http'
-import { URL } from 'url'
 
 import config from './config'
 import { app } from './webApp'
-import { wss } from './wss'
-import type { WSConnectionParam } from './wss/types'
-import { verifyAuthToken } from './utils'
+import { attachWSToServer } from 'app/wss/attachToServer'
 
 export const server = createServer(app)
-
-server.on('upgrade', function upgrade(request, socket, head) {
-  if (!request.url) {
-    return destroy(400, 'Bad request')
-  }
-
-  const { pathname } = new URL(`http://localhost${request.url}`)
-
-  if (pathname !== '/socket') {
-    return destroy(404, 'Not Found')
-  }
-
-  const authHeader = request.headers.authorization
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return destroy(401, 'Unauthorized')
-  }
-
-  const [playerId, secretToken] = authHeader.substring(7).split(':')
-  if (!verifyAuthToken(playerId, secretToken)) {
-    return destroy(401, 'Authentication failed')
-  }
-
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    const param: WSConnectionParam = {
-      playerId,
-    }
-    wss.emit('connection', ws, param)
-  })
-
-  function destroy(errorCode: number, errorMessage: string) {
-    socket.write(
-      `HTTP/1.1 ${errorCode} ${errorMessage}\r\n\r\n`,
-      'utf-8',
-      () => {
-        socket.destroy()
-      },
-    )
-  }
-})
+attachWSToServer(server)
 
 server.listen(config.port, () => {
   const address = server.address() as AddressInfo
