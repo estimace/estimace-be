18204a2f7d4032d6e2af0eecb73109627652e111
Tahereh Pourkhalil <taherehpourkhalil.m@gmail.com>
2024-09-28 11:47:45 +0200

    fix: upgrade express to solve the body parser vulnerability

--- src/webApp/index.ts
+++ src/webApp/index.ts
@@ -7,7 +7,6 @@ import { init as initRoutes } from 'app/webApp/routes'
 export const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 
 // logs all requests in verbose mode
 if (config.verbose) {
